5a4548dbdc135f3ef8c635b89c98875218714145
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2015-09-11 11:36:29 +0300

    logic added

--- hello_world.php
+++ hello_world.php
@@ -6,3 +6,20 @@
 	echo $first_name." ".$last_name;
 
 ?>
+
+<br>
+
+<?php
+
+	$age  = 19;
+	
+	//kui on väiksem kui 18, siis on alaealine. Kui on suurem kui 18, siis täisealine
+	if ($age < 18){
+		//tõene
+		echo "alaealine";
+	}	else	{
+		//väär
+		echo "täisealine";
+	}
+
+?>
